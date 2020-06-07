5da5d426beaff61807db3341c59ae55c1286c444
yeongminkim <oinochoe@naver.com>
2020-06-07 21:36:01 +0900

    js 구현 로드맵 설계하기

---
+++ js/index.js
@@ -0,0 +1,10 @@
+async function main() {
+    // 버튼에 이벤트 연결
+    // 토큰 체크
+    // 토큰으로 서버에서 나의 정보 받아오기
+    // 나의 책을 서버에서 받아오기
+    // 받아온 책을 그리기
+}
+
+// dom 로드 되면 main 실행
+document.addEventListener('DOMContentLoaded', main);
