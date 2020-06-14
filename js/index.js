3c1a0082cd5a1da4c06262d13478f651b30d9015
yeongminkim <oinochoe@naver.com>
2020-06-14 23:00:38 +0900

    login 주소 수정

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ async function getUserByToken(token) {
 async function logout() {
     const token = getToken();
     if (token === null) {
-        location.assign('/login');
+        location.assign('/login.html');
         return;
     }
     try {
@@ -32,7 +32,7 @@ async function logout() {
         console.log('logout error', error);
     } finally {
         localStorage.clear();
-        location.assign('/login');
+        location.assign('/login.html');
     }
 }
 
@@ -53,7 +53,7 @@ async function getBooks(token) {
 async function deleteBook(bookId) {
     const token = getToken();
     if (token === null) {
-        location.assign('/login');
+        location.assign('/login.html');
         return;
     }
     await axios.delete(`https://api.marktube.tv/v1/book/${bookId}`, {
@@ -122,7 +122,7 @@ async function main() {
     // 토큰 체크
     const token = getToken();
     if (token === null) {
-        location.assign('/login');
+        location.assign('/login.html');
         return;
     }
 
@@ -130,7 +130,7 @@ async function main() {
     const user = await getUserByToken(token);
     if (user === null) {
         localStorage.clear();
-        location.assign('/login');
+        location.assign('/login.html');
         return;
     }
 
